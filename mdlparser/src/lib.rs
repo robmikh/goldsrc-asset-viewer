506a5166bbf2ed51c991d0a777dcbd9316f263df
Robert Mikhayelyan <rob.mikh@outlook.com>
2019-06-10 15:19:25 -0700

    better mdl texture parsing (semi-working)

--- mdlparser/src/lib.rs
+++ mdlparser/src/lib.rs
@@ -5,6 +5,7 @@ extern crate byteorder;
 
 use std::io::{Cursor, BufReader, Read, Seek, SeekFrom};
 use std::fs::File;
+use std::path::{Path, PathBuf};
 use std::str;
 
 use byteorder::{LittleEndian, ReadBytesExt};
@@ -25,11 +26,24 @@ pub struct MdlFile {
 
 #[derive(Copy, Clone, Deserialize)]
 struct TextureHeader {
-    name_offset: u32,
+    name: [[u8; 8]; 8],
     flags: u32,
     width: u32,
     height: u32,
     offset: u32,
+}
+
+impl TextureHeader {
+    fn name(&self) -> &[u8; 64] {
+        unsafe { std::mem::transmute(&self.name) }
+    }
+
+    fn name_string(&self) -> String {
+        let name = self.name();
+        let name_string = String::from_utf8_lossy(name);
+        let name_string = name_string.trim_matches(char::from(0));
+        name_string.to_string()
+    }
 }
 
 #[derive(Copy, Clone, Deserialize)]
@@ -43,7 +57,7 @@ struct MdlHeader {
     eye_position: [f32; 3],
     hull_min: [f32; 3],
     hull_max: [f32; 3],
-    
+
     view_bbmin: [f32; 3],
     view_bbmax: [f32; 3],
 
@@ -106,32 +120,28 @@ impl MdlFile {
         let file_size = file.metadata().unwrap().len();
         let mut file = BufReader::new(file);
 
-        let header : MdlHeader = bincode::deserialize_from(&mut file).unwrap();
+        let mut header : MdlHeader = bincode::deserialize_from(&mut file).unwrap();
         let file_name = header.name_string();
 
-        let num_textures = header.texture_count as usize;
-        let mut textures = Vec::with_capacity(num_textures);
-        for i in 0..num_textures {
-            file.seek(SeekFrom::Start(header.texture_offset as u64)).unwrap();
-            let texture_header: TextureHeader = bincode::deserialize_from(&mut file).unwrap();
-            file.seek(SeekFrom::Start(texture_header.name_offset as u64)).unwrap();
+        let textures = if header.texture_count == 0 {
+            let mut texture_mdl_path = PathBuf::from(mdl_path);
+            let file_stem = texture_mdl_path.file_stem().unwrap().to_str().unwrap();
+            texture_mdl_path.set_file_name(format!("{}t.mdl", file_stem));
+            let texture_mdl_path = texture_mdl_path.into_os_string();
+            let texture_mdl_path = texture_mdl_path.into_string().unwrap();
 
-            let mut name_data = Vec::new();
-            while (true) {
-                let current_char = file.read_u8().unwrap();
+            let file = File::open(texture_mdl_path).unwrap();
+            let file_size = file.metadata().unwrap().len();
+            let mut file = BufReader::new(file);
+            let texture_header: MdlHeader = bincode::deserialize_from(&mut file).unwrap();
 
-                if current_char == 0 {
-                    break;
-                }
-
-                name_data.push(current_char);
-            }
-
-            let name_string = str::from_utf8(&name_data).unwrap();
-            textures.push(MdlTexture {
-                name: name_string.to_string(),
-            });
-        }
+            header.texture_count = texture_header.texture_count;
+            header.texture_offset = texture_header.texture_offset;
+            header.texture_data_index = texture_header.texture_data_index;
+            read_textures(&mut file, &texture_header)
+        } else {
+            read_textures(&mut file, &header)
+        };
 
         file.seek(SeekFrom::Start(0)).unwrap();
         let mut file_data = vec![0u8; file_size as usize];
@@ -144,4 +154,19 @@ impl MdlFile {
             raw_data: file_data,
         }
     }
+}
+
+fn read_textures<T: Read + Seek>(mut reader: &mut T, header: &MdlHeader) -> Vec<MdlTexture> {
+    let num_textures = header.texture_count as usize;
+    let mut textures = Vec::with_capacity(num_textures);
+    reader.seek(SeekFrom::Start(header.texture_offset as u64)).unwrap();
+    for i in 0..num_textures {
+        let texture_header: TextureHeader = bincode::deserialize_from(&mut reader).unwrap();
+
+        let name_string = texture_header.name_string();
+        textures.push(MdlTexture {
+            name: name_string.to_string(),
+        });
+    }
+    textures
 }
