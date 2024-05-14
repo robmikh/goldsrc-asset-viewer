e2c8d6c976e0a1d8c5a259c45f2659988e79ed34
robmikh <rob.mikh@outlook.com>
2024-05-14 02:30:00 -0700

    update sampler

--- src/rendering/bsp.rs
+++ src/rendering/bsp.rs
@@ -446,12 +446,12 @@ fn create_depth_texture(device: &wgpu::Device, config: &wgpu::SurfaceConfigurati
     let view = texture.create_view(&wgpu::TextureViewDescriptor::default());
     let sampler = device.create_sampler(
         &wgpu::SamplerDescriptor {
-            address_mode_u: wgpu::AddressMode::ClampToEdge,
-            address_mode_v: wgpu::AddressMode::ClampToEdge,
+            address_mode_u: wgpu::AddressMode::MirrorRepeat,
+            address_mode_v: wgpu::AddressMode::MirrorRepeat,
             address_mode_w: wgpu::AddressMode::ClampToEdge,
             mag_filter: wgpu::FilterMode::Linear,
             min_filter: wgpu::FilterMode::Linear,
-            mipmap_filter: wgpu::FilterMode::Nearest,
+            mipmap_filter: wgpu::FilterMode::Linear,
             compare: Some(wgpu::CompareFunction::LessEqual),
             lod_min_clamp: 0.0,
             lod_max_clamp: 100.0,
